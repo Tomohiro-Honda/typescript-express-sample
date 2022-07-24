1e520544a19d5675d502f7b621bdc65b2c9eca2d
Tomohiro-Honda-ML <t.honda@mighty-link.com>
2022-07-24 10:48:43 +0000

    create POST method user add

--- src/config/mysqlconf.ts
+++ src/config/mysqlconf.ts
@@ -1,7 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const MYSQL_CONFIG = {
-  HOST: process.env.DB_HOST || 'db',
-  PORT: Number(process.env.DB_PORT) || 3306,
-  USER: process.env.USER || 'root',
-  PASSWORD: process.env.DB_PASSWORD || 'root',
-  DATABASE: process.env.DB_DATABASE_NAME || 'next-api-db',
+  HOST: process.env.DB_HOST,
+  PORT: Number(process.env.DB_PORT),
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DATABASE: process.env.DB_DATABASE_NAME,
+  CONNECTION_LIMIT: Number(process.env.DB_CONNECTION_LIMIT),
 };
